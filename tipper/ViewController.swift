44b445e7b078afe17256fcec0e91930f07430c44
Ben <ben@Hoangs-MacBook-Pro-3.local>
2020-01-23 18:18:52 -0800

    Initial commit

---
+++ tipper/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  tipper
+//
+//  Created by Ben on 1/23/20.
+//  Copyright © 2020 Ben. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func onTap(_ sender: Any) {
+        view.endEditing(true)
+    }
+    
+    @IBAction func calculateTip(_ sender: Any) {
+        let bill = Double(billField.text!) ?? 0
+        let tipPercents = [0.15,0.18,0.2]
+        
+        let tip = bill * tipPercents[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
+}
+
